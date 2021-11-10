7b841882482a715721bc41de7e6ac498bb390757
Sean Danielson <sdanielson719@gmail.com>
2021-11-10 12:41:45 -0500

    Added check if the source/dest port is 80

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from netfilterqueue import NetfilterQueue
+import scapy.all as scapy
+from scapy.layers.inet import IP, TCP
 
 def print_and_accept(pkt):
-    print("here")
-    print(pkt.get_payload())
-    pkt.accept()
+    packet = scapy.IP(pkt.get_payload())
+    if packet.haslayer(TCP) and (packet[TCP].sport == 80 or packet[TCP].dport == 80):
+        pkt.drop()
+    else:
+        pkt.accept()
 
 nfqueue = NetfilterQueue()
 nfqueue.bind(1, print_and_accept)
 try:
-    print("Here")
     nfqueue.run()
 except KeyboardInterrupt:
     print('')
+    nfqueue.unbind()
 
-nfqueue.unbind()
