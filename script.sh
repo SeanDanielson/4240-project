08b08e77f0582450534a95179791d87d0c38b6f4
cdwise <56054770+cdwise@users.noreply.github.com>
2021-11-11 18:04:03 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,8 @@ COMMAND=""
 if [pgrep -x "$COMMAND"]
 then
   killall COMMAND
+  sudo iptables -F
 else
   IPADDRESS=/sbin/ifconfig
-  iptables -I INPUT -d $IPADDRESS -j NFQUEUE --queue-num 1
+  iptables -I FORWARD -d $IPADDRESS -j NFQUEUE --queue-num 1
 fi
