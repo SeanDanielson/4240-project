8a5a8baa2712eed3a8b69f87fd4f03cff281928c
cdwise <56054770+cdwise@users.noreply.github.com>
2021-11-11 17:36:06 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,4 +2,11 @@
 #compile code
 python3 main.py &
 #run code
-iptables -I INPUT -d 192.168.0.0/24 -j NFQUEUE --queue-num 1
+COMMAND=""
+if [pgrep -x "$COMMAND"]
+then
+  killall COMMAND
+else
+  IPADDRESS=/sbin/ifconfig
+  iptables -I INPUT -d $IPADDRESS -j NFQUEUE --queue-num 1
+fi
