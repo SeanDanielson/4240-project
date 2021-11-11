0d97447d65cb2e36c625bc034dbeb9a82e1bf2ae
cdwise <56054770+cdwise@users.noreply.github.com>
2021-11-11 18:19:08 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ python3 main.py &
 COMMAND="python3"
 if pgrep -x $COMMAND
 then
-  killall COMMAND
+  killall $COMMAND
   sudo iptables -F
 else
   IPADDRESS=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
