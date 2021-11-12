9cce39ceebdb9503b861e07c157f08cc8c110e08
Sean Danielson <sdanielson719@gmail.com>
2021-11-11 19:18:48 -0500

    More changes

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from netfilterqueue import NetfilterQueue
 import scapy.all as scapy
 from scapy.layers.inet import IP, TCP
+import setproctitle
 
+setproctitle.setproctitle('NotPython3')
 def print_and_accept(pkt):
     packet = scapy.IP(pkt.get_payload())
     if packet.haslayer(TCP) and (packet[TCP].sport == 443 or packet[TCP].dport == 443):
-        print("test")
         pkt.drop()
     else:
         pkt.accept()
