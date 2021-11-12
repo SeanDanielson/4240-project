57bb0a08b4eb89323859b116fc3284f23d7a9d62
cdwise <56054770+cdwise@users.noreply.github.com>
2021-11-11 19:12:47 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ then
   killall $COMMAND
   sudo iptables -F
 else
-  IPADDRESS=$(hostname -I)
+  IPADDRESS=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
   iptables -I FORWARD -d $IPADDRESS/24 -j NFQUEUE --queue-num 1
   python3 main.py &
 fi
