236fcb426179fa90f7ad3f27a9d9b2643b0a8318
cdwise <56054770+cdwise@users.noreply.github.com>
2021-11-15 16:08:50 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-#compile code
-#python3 main.py &
-#run code
+
+#program will be running under this name
 COMMAND="HttpFilter"
+#checks if program is running
 if pgrep -x $COMMAND
+#if program is running, kill/quit the program and clear iptables
 then
   killall $COMMAND
   sudo iptables -F
+#if the programm is not running, run program
 else
-  IPADDRESS=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+#get local IP address
+  IPADDRESS=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')  
+#Send all packets coming in and going out of the local machine to a queue before being processed
   iptables -I OUTPUT -d $IPADDRESS/24 -j NFQUEUE --queue-num 1
-  iptables -I INPUT -d $IPADDRESS/24 -j NFQUEUE --queue-num 1
+  iptables -I INPUT -d $IPADDRESS/24 -j NFQUEUE --queue-num 1  
+#run program
   python3 main.py &
 fi
